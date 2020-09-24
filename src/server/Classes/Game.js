530063dbc0c9e6fe8b5a77fbaed06faed79c086c
samofoke <samofoke@student.wethinkcode.co.za>
2020-09-24 12:12:20 +0200

    edited the board, game and player classes

--- src/server/Classes/Game.js
+++ src/server/Classes/Game.js
@@ -1,12 +1,16 @@
+import Piece from './tetroPieces';
+
 class Game {
     static gcnt = 0;
     static mxp = 4;
+    static npcnt = 10;
 
     constructor(p) {
         this.id = Game.gcnt++;
         this.g = p;
         this.ifPlayer = false;
         this.players = [p];
+        this.plist = [];
     }
 
     getInformation() {
@@ -15,6 +19,32 @@ class Game {
             playerName: this.g.name,
             plcnt: this.players.length
         };
+    }
+
+    TheGame() {
+        Piece
+        this.addpiece();
+        //create a board for each player
+        this.players.forEach(ps => {
+            ps.bord = new BoardGame({
+                gcallback: (bord) => {
+                    if (bord.pcp - this.plist.length < 5) {
+                        this.plist.push(Piece.randomNumberPieces, bord.pcp + 5).map(ps => {
+                            return new Piece(ps);
+                        });
+                    };
+                    bord.pcp += 5;
+                }
+            });
+        });
+    }
+
+    startGame() {
+
+    }
+
+    addpiece() {
+        this.plist.push(Piece.randomNumberPieces(Game.npcnt));
     }
 }
 
--- src/server/Classes/board.js
+++ src/server/Classes/board.js
@@ -6,17 +6,20 @@ class BoardGame {
     constructor(res) {
         let defaultBoard = {
             size: {x: 10, y : 20},
-            plst: []
+            plst: [],
+            pcp: 0,
+            gcallback: () => {console.log("no call back yet...")}
         }
         res = {...defaultBoard, ...res};
         this.size = res.size;
-        this.cs = new Array(this.size.x);
+        this.cs = new Array(this.size.y);
         //looping through the cordinates
-        for (let index = 0; index < this.size.x; index++) {
-            this.cs[index] = new Array(this.size.y).fill(0x0);
+        for (let index = 0; index < this.size.y; index++) {
+            this.cs[index] = new Array(this.size.x).fill(0x0);
         }
         this.actps = null;
         this.plst = res.plst;
+        this.gcallback = res.gcallback;
     }
 
     //function to add pieces
@@ -35,36 +38,39 @@ class BoardGame {
         this.actps = this.plst.shift();
         if (!this.actps){
             console.log("[board.js] error: no next piece");
+        }else {
+            this.gcallback(this);
         }
     }
 
     
 
     //the tries to check rotate the piece
-    checkrotate(ps = this.actps) {
+    checkrotate(ps) {
         console.log("[board.js] checks for rotate");
         if (!ps) {
             console.log("\t to piece");
             return;
         }
         //checks collision on the board and tries to rotate
-        let rot = new Piece(ps);
-        rot.rotate();
-        console.log("the piece rotates", rot);
-        let place = this.overlap(rot);
-        console.log("can place a piece");
-        if (place) {
-            this.actps = rot;
-            return rot;
-        }else {
-            rot = this.checksmoves(rot);
-        }
-        return ps;
+        //let rot = new Piece(ps);
+        ps.rotate();
+        console.log("the piece rotates", ps);
+        // let place = this.overlap(rot);
+        // console.log("can place a piece");
+        // if (place) {
+        //     this.actps = rot;
+        //     return rot;
+        // }else {
+        //     rot = this.checksmoves(rot);
+        // }
+        // return ps;
+        return this.overlap(ps);
     }
 
     //the checks for moves and moves a piece and active pieces
 
-    checksmoves(v, pms = this.actps) {
+    checksmoves(pms, v) {
         console.log("[board.js] moves a piece, v", v);
         if (!pms) {
             console.log("\t to piece");
@@ -74,15 +80,16 @@ class BoardGame {
             console.log("\t invalid move");
             return pms;
         }
-        let m = new Piece(pms);
-        m.updateCordinates(v);
-        let place = this.overlap(m);
-        console.log("can move a piece", place);
-        if (place) {
-            if (this.actps == pms) {
-                this.actps = m;
-            }
-        } 
+        //let m = new Piece(pms);
+        pms.updateCordinates(v);
+        // let place = this.overlap(m);
+        // console.log("can move a piece", place);
+        // if (place) {
+        //     if (this.actps == pms) {
+        //         this.actps = m;
+        //     }
+        // }
+        return this.overlap(pms); 
     }
 
     //the function for valid pieces on the board
@@ -92,12 +99,17 @@ class BoardGame {
         if (!psx) {
             return false;
         }
-        for (let x = 0; x < 4; x++) {
-            for (let y = 0; y < 4; y++) {
-                if (psx.cs[x][y] != 0x0) {
+        for (let y = 0; y < 4; y++) {
+            for (let x = 0; x < 4; x++) {
+                if (psx.cs[y][x] != 0x0) {
                     //if the piece is null outside the board
-                    if (psx.crd.x + x >= this.size.x || psx.crd.x + x < 0 || psx.crd.y + y >= this.size.y || psx.crd.y + y < 0) {
-                        console.log("[board.js] a piece overlaps at: ", {x: psx.crd.x + x, y: psx.crd.y + y}, ", piece", psx.cs[x][y], "board: ", this.cs[psx.crd.x + x][psx.crd.y + y]);
+                    if (psx.crd.x + x >= this.size.x || psx.crd.x + x < 0 || psx.coords.y + y >= this.size.y || psx.crd.y + y < 0) {
+						console.log("[board.js] piece out of scope. crd: ", {x: psx.crd.x + x, y: psx.crd.y + y});
+                        return false;
+                    }
+                    //if non-null piece is on a null board
+                    else if (this.cs[psx.crd.y + y][psx.crd.x + x] != 0x0 && psx.cs[y][x] != 0x0) {
+                        console.log("[board.js] a piece overlaps at: ", {x: psx.crd.x + x, y: psx.crd.y + y}, ", piece", psx.cs[y][x], "board: ", this.cs[psx.crd.y + y][psx.crd.x + x]);
                         return false;
                     }
                 }
@@ -113,10 +125,10 @@ class BoardGame {
         if (!p) {
             return;
         }
-        for (let x = 0; x < 4; x++){
-            for (let y = 0; y < 4; y++) {
-                if (p.cs[x][y] != 0x0) {
-                    this.cs[x][y] = p.cs[x][y];
+        for (let y = 0; y < 4; y++){
+            for (let x = 0; x < 4; x++) {
+                if (p.cs[y][x] != 0x0) {
+                    this.cs[y][x] = p.cs[y][x];
                 }
             }
         }
@@ -127,23 +139,91 @@ class BoardGame {
         for (let y = 0; y < this.size.y; y++) {
             let l = "";
             for (let x = 0; x < this.size.x; x++) {
-                l += (this.cs[x][y] ? "1" : "0") + " ";
+                l += (this.cs[y][x] ? "1" : "0") + " ";
             }
             console.log(l);
         }
     }
 
     moveRight() {
-
+        if (!this.actps) {
+            return ;
+        }
+        let mv = new Piece(this.actps);
+        let cm = this.checksmoves(mv, {x: 1, y: 0});
+        if (cm) {
+            this.actps = mv;
+        }
     }
     moveLeft() {
+        if (!this.actps) {
+            return ;
+        }
+        let mv = new Piece(this.actps);
+        let cm = this.checksmoves(mv, {x: -1, y: 0});
+        if (cm) {
+            this.actps = mv;
+        }
 
     }
     moveDown() {
+        if (!this.actps) {
+            return ;
+        }
+        let mv = new Piece(this.actps);
+        let cm = this.checksmoves(mv, {x: 0, y: 1});
+        if (cm) {
+            this.actps = mv;
+        }
 
     }
     rotate() {
+        console.log("[board.js] rotate piece");
+        if (!this.actps) {
+            return ;
+        }
+        let rotps = new Piece(this.actps);
+        let cpls = this.checkrotate(rotps);
+        if (!cpls) {
+            console.log("\t move left 1");
+            cpls = this.checksmoves(rotps, {x: -1, y: 0});
+        }
+        if (!cpls) {
+            console.log("\t move left 2");
+            cpls = this.checksmoves(rotps, {x: -1, y: 0});
+        }
+        if (!cpls) {
+            console.log("\t move right 1");
+            cpls = this.checksmoves(rotps, {x: 3, y: 0});
+        }
+        if (!cpls) {
+            console.log("\t move right 2");
+            cpls = this.checksmoves(rotps, {x: 1, y: 0});
+        }
+        if (!cpls) {
+            console.log("\t move up 1");
+            cpls = this.checksmoves(rotps, {x: -2, y: 0});
+        }
+        if (!cpls) {
+            console.log("\t move up 2");
+            cpls = this.checksmoves(rotps, {x: 0, y: 1});
+        }
+        if (cpls) {
+            console.log("\t The rotate");
+            this.actps = rotps;
+        }
+    }
 
+    getblocks() {
+        let cs = JSON.parse(JSON.stringify(cs));
+        for (let y = 0; y < 4; y++) {
+            for (let x = 0; x < 4; x++) {
+                if (this.actps.cs[y][x] != 0x0) {
+                    cs[this.actps.crd.y + y][this.actps.crd.x + x] = this.actps.cs[y][x];
+                }
+            }
+        }
+        return cs;
     }
 }
 
@@ -158,6 +238,8 @@ let bord = new BoardGame(
     }
 );
 
+bord.nextPiece();
+console.log(bord.getblocks());
 //console.log("bord: ", bord);
 
 // let ps = new Piece(1);
