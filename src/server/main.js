034c0b7a1fb459392d0c965804063c0fd03026e3
samofoke <samofoke@student.wethinkcode.co.za>
2020-10-01 16:19:53 +0200

    edited the port

--- src/server/main.js
+++ src/server/main.js
@@ -1,3 +1,3 @@
 import params  from '../../params'
 import * as server from './index'
-server.create(params.server).then( () => console.log('not yet ready to play tetris with U ...') )
+server.create(params.server).then( () => console.log('not yet ready to play tetris with U ...port:0.0.0.0:3004') )
